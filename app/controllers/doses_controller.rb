92c1e00adb489f367ead3c03be2c104dd46453ee
Jess Pring <jessicapring@outlook.com>
2020-08-13 16:49:13 +0100

    Dose new/create buggy

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,13 +2,16 @@ class DosesController < ApplicationController
   def new
     @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredient = Ingredient.find(params[:ingredient_id])
   end
 
   def create
     @dose = Dose.new(dose_params)
 
     @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredient = Ingredient.find(params[:ingredient_id])
     @dose.cocktail_id = @cocktail
+    # @dose.ingredient_id = @ingredient
 
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -20,7 +23,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@dose.cocktail)
+    redirect_to cocktail_path(@dose.cocktail_id)
   end
 
   private
